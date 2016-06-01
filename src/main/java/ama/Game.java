d7d567beef6ceaaf5ba6837d051dfc0e59a220dc
ama_nobu <nqf33359@nifty.com>
2016-06-01 14:30:51 +0900

    JPA Test Add

--- src/main/java/ama/Game.java
+++ src/main/java/ama/Game.java
@@ -1,14 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ama;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  *
  * @author ama
  */
-public class Game {
-    
+@Entity
+public class Game implements Serializable {
+
+    private Long id;
+    private String title;
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Game(String title) {
+        this.title = title;
+    }
+
+    public Game() {
+    }
+
+    @Override
+    public String toString() {
+        return "Game@" + hashCode() + "[id = " + id + "; title = " + title + "]";
+    }
+
 }
